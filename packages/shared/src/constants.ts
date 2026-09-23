b05d0bc584f84a5c63508289f59e18fd0b9257bf
zyfshr <827248690@qq.com>
2026-09-24 05:34:14 +0800

    fix: recognize UnionKey Bluetooth device names

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -96,7 +96,8 @@ export const isOnekeyDevice = (name: string | null, id?: string): boolean => {
 
   // 过滤 BixinKeyxxx 和 Kxxxx 和 Txxxx
   // i 忽略大小写模式
-  const re = /(BixinKey\d{10})|(K\d{4})|(T\d{4})|(Touch\s\w{4})|(Pro\s\w{4})/i;
+  const re =
+    /(UnionKey[\w-]*)|(OneKey[\w-]*)|(BixinKey\d{10})|(K\d{4})|(T\d{4})|(Touch\s\w{4})|(Pro\s\w{4})/i;
   if (name && re.exec(name)) {
     return true;
   }
